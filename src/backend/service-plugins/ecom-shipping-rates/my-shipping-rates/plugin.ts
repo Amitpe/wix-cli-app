8168e62dd1d84fc1d1b147178104b504803d18fc
amitp <amitp@wix.com>
2024-11-03 17:00:27 +0200

    implement shipping rates SPI

--- src/backend/service-plugins/ecom-shipping-rates/my-shipping-rates/plugin.ts
+++ src/backend/service-plugins/ecom-shipping-rates/my-shipping-rates/plugin.ts
@@ -1,9 +1,18 @@
-import { shippingRates } from '@wix/ecom/service-plugins';
+import {shippingRates} from '@wix/ecom/service-plugins';
 
 shippingRates.provideHandlers({
-  getShippingRates: async ({ request, metadata }) => {
-    return {
-      shippingRates: [],
-    };
-  },
+    getShippingRates: async ({request, metadata}) => {
+        console.log('Hello from getShippingRates')
+
+        const cost: shippingRates.ShippingPrice = {price: '10', currency: 'USD'}
+        const shippingOption: shippingRates.ShippingOption = {
+            code: 'shippingCode',
+            title: 'My custom shipping option',
+            cost: cost
+        }
+        const shippingRates: shippingRates.ShippingOption[] = [shippingOption];
+        const shippingRatesResponse: shippingRates.GetShippingRatesResponse = {shippingRates};
+
+        return shippingRatesResponse;
+    },
 });
