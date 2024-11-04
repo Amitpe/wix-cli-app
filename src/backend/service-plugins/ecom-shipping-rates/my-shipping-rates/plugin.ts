c015a93425a9ea1fde7c5f8a6f28f7faf2a8b3f1
amitp <amitp@wix.com>
2024-11-04 09:53:54 +0200

    log the request, shorten that shipping title

--- src/backend/service-plugins/ecom-shipping-rates/my-shipping-rates/plugin.ts
+++ src/backend/service-plugins/ecom-shipping-rates/my-shipping-rates/plugin.ts
@@ -2,12 +2,12 @@ import {shippingRates} from '@wix/ecom/service-plugins';
 
 shippingRates.provideHandlers({
     getShippingRates: async ({request, metadata}) => {
-        console.log('Hello from getShippingRates')
+        console.log('Hello from getShippingRates. Request is: ', request)
 
         const cost: shippingRates.ShippingPrice = {price: '10', currency: 'USD'}
         const shippingOption: shippingRates.ShippingOption = {
             code: 'shippingCode',
-            title: 'My custom shipping option',
+            title: 'Custom shipping option',
             cost: cost
         }
         const shippingRates: shippingRates.ShippingOption[] = [shippingOption];
