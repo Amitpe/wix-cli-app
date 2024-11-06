22d98e2cafb748ad0149a7309f483f238890d921
amitp <amitp@wix.com>
2024-11-06 15:41:11 +0200

    add to collection after order is placed - WIP

--- src/backend/events/order-created-handler/event.ts
+++ src/backend/events/order-created-handler/event.ts
@@ -1,5 +1,35 @@
-import { orders } from "@wix/ecom";
+import {orders} from "@wix/ecom";
+import {items} from "@wix/data";
+import { auth } from "@wix/essentials";
 
-orders.onOrderCreated((event) => {
-    console.log('Hello from order created: ', event)
+
+// TODO refactor to Consts.ts
+const MY_CLI_APP_ID = 'aa3a54aa-4433-433a-94e8-7a34053dd1c7';
+
+orders.onOrderCreated(async (event) => {
+    // Check if the order contain my special shipping option
+    // If yes, add it to the special order collection
+
+    console.log('Hello from Order Created Event')
+    console.log('Event is:\n ', event)
+
+
+    const selectedShippingOptionCode = event.entity.shippingInfo?.code
+    const carrierId = event.entity.shippingInfo?.carrierId
+
+    if (carrierId === MY_CLI_APP_ID && selectedShippingOptionCode === 'customShippingCode') {
+        console.log('Item is:\n ', event)
+        const item: items.InsertDataItemOptions = {
+            dataCollectionId: 'special-orders',
+            dataItem: {
+                data: {
+                    orderId: event.entity._id,
+                    shippingOptionId: 'special-orders',
+                    deliveryAddress: event.entity.shippingInfo?.logistics?.shippingDestination?.address,
+                    shippingRate: event.entity.shippingInfo?.cost?.price?.amount
+                },
+            }
+        }
+        await auth.elevate(items.insertDataItem)(item)
+    }
 });
